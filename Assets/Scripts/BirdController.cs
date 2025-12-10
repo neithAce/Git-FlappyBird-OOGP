3319c876cf741355581fbb09afca7db62da6c507
neithAce <davecandela38@gmail.com>
2025-12-10 17:22:30 +0700

    Bird Controller, Obstacle Base
    membuat scriptnya

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    private Rigidbody2D _rb2d;
+    private float _flapForce = 5f;
+    private bool _isAlive = true;
+
+    [SerializeField] private float _yBounds = -5f;
+
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Flap();
+        }
+
+        if(_isAlive && transform.position.y < _yBounds)
+        {
+            Die();
+        }
+    }
+
+    public void Flap()
+    {
+        if (_isAlive) {
+            _rb2d.linearVelocity = Vector2.zero;
+            _rb2d.linearVelocity = Vector2.up * _flapForce;
+        }
+    }
+
+    public void Die()
+    {
+       if(!_isAlive) return;
+       _isAlive = false;
+        Debug.Log("Bird died!");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Die();
+    }
+}
---
+++ Assets/Scripts/ObstacleBase.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObstacleBase : MonoBehaviour
+{
+    [SerializeField] protected float speed = 2f;
+    [SerializeField] protected float destroyXPosition = -10f;
+
+    public abstract void Move();
+
+    protected void CheckDestroy()
+    {
+        if (transform.position.x < destroyXPosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void Update()
+    {
+        Move();
+        CheckDestroy();
+    }
+}
